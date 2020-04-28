a28240a0593a0d3ef5830ace1815ff9ca30c35e7
Mosh Feu <moshfeu@gmail.com>
2020-04-28 19:07:53 +0300

    mobile - phase 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const handler = require('serve-handler');
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http').createServer((request, response) => handler(request, response));
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
@@ -18,5 +19,20 @@ io.on('connection', (socket) => {
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log(`listening on ${getIPAddress()}:3000`);
 });
+
+function getIPAddress() {
+  const interfaces = require('os').networkInterfaces();
+  for (const devName in interfaces) {
+    const iface = interfaces[devName];
+
+    for (let i = 0; i < iface.length; i++) {
+      const alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+
+  return '0.0.0.0';
+}
